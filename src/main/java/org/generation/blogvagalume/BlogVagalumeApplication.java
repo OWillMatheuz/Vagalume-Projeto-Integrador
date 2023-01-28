262c1beab394a33e3fe633b9e93f9cc7699b55f5
Will Matheus <dev.owillmatheus@gmail.com>
2023-01-28 15:17:22 -0300

    versão 1.0

---
+++ src/main/java/org/generation/blogvagalume/BlogVagalumeApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogvagalume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogVagalumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogVagalumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/blogvagalume/BlogVagalumeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogvagalume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogVagalumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
