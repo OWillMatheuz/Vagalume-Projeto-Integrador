51a121b4aa318615f0ab1081000b8877fa8101ef
Will Matheus <119905167+OWillMatheuz@users.noreply.github.com>
2023-01-29 18:28:54 -0300

    Add files via upload

--- src/main/java/org/generation/blogvagalume/repository/TemaRepository.java
+++ src/main/java/org/generation/blogvagalume/repository/TemaRepository.java
@@ -2,13 +2,12 @@ package org.generation.blogvagalume.repository;
 
 import java.util.List;
 
-import org.generation.blogvagalume.model.PostagemVagalume;
+import org.generation.blogvagalume.model.TemaVagalume;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostagemRepository extends JpaRepository<PostagemVagalume, Long> {
-	public List<PostagemVagalume> findAllByTituloContainingIgnoreCase
-	(@Param("titulo") String titulo);
+public interface TemaRepository extends JpaRepository<TemaVagalume, Long> {
+	public List<TemaVagalume> findAllByDescricaoContainingIgnoreCase(@Param("descricao")String descricao);
 }
