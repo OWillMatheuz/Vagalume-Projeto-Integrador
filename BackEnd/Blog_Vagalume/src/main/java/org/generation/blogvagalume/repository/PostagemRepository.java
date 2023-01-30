102ef303867f60d7eb1adad727388eb9f0ca37aa
Will Matheus <dev.owillmatheus@gmail.com>
2023-01-30 10:43:30 -0300

    versao 1.0

---
+++ BackEnd/Blog_Vagalume/src/main/java/org/generation/blogvagalume/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blogvagalume.repository;
+
+import java.util.List;
+
+import org.generation.blogvagalume.model.PostagemVagalume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<PostagemVagalume, Long> {
+	public List<PostagemVagalume> findAllByTituloContainingIgnoreCase
+	(@Param("titulo") String titulo);
+}
